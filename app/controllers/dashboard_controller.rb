87ed0e8b98d13bb1c0b42ae745fa2ea64d9665e6
Dan Melton <melton.dan@gmail.com>
2011-05-25 13:46:11 -0700

    Adding content for views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,13 @@
 class DashboardController < ApplicationController
   
   def index
+    @org = Org.first
+    @projects = Project.all(:include => [:commits, :coders])
+    @projects_by_commit_size = @projects.collect { |x| [x, x.commits.size]}.sort_by {|x| x[1]}.reverse    
+    @projects_by_coder_size = @projects.collect { |x| [x, x.coders.size]}.sort_by {|x| x[1]}.reverse    
+    @commits = Commit.all(:include => [:coder, :project])
+    @coders = Coder.all(:include => [:projects, :commits])
+    @coders_by_commit_size = @coders.collect { |x| [x, x.commits.size]}.sort_by {|x| x[1]}.reverse    
   end
   
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -80,5 +80,6 @@ class Project < ActiveRecord::Base
     end
     
   end
+
   
 end
