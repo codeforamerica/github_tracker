5f132f33fd72703ab5d3d7cffacbb7d38e554fd3
Ryan Resella <ryan@codeforamerica.org>
2012-04-26 15:53:37 -0700

    Added config initialize for heroku

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,8 @@ module GithubTracker
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.stylesheets false
       g.test_framework :rspec
